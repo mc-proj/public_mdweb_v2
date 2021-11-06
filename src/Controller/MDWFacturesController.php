73691b82ff4c3f9c3cb56ae110f85768fd4c6cae
mc-proj <mcretenier@gmx.fr>
2021-11-06 12:13:48 +0100

    journee 6 novembre 2021

--- src/Controller/MDWFacturesController.php
+++ src/Controller/MDWFacturesController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MDWFacturesRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use DateTime;
 
 #[Route('/factures')]
 
@@ -29,7 +30,44 @@ class MDWFacturesController extends AbstractController
                 "user" => $this->getUser(),
             ]);
 
-            $facture = $normalizer->normalize($facture, 'json',  ['groups' => 'read:facture:MDWFacture']);
+            $date_achat = $facture->getDateCreation(); //test
+
+            $facture = $normalizer->normalize($facture, 'json',  ['groups' => 'read:facture:MDWFacture']); //ori
+
+            //test begin
+            //dd($facture);
+            $factures_produits = $facture['produit'];
+            //dd($factures_produits);
+            $factures_produits_edite = [];
+
+            foreach($factures_produits as $facture_produit) {
+                //dd($facture_produit);
+                $produit = $facture_produit['produit'];
+                $infos_promo = [
+                    'en_promo' => false,
+                    'tarif_actif' => $produit['tarif']
+                ];
+
+                if(new DateTime($produit['date_debut_promo']) <= $date_achat && new DateTime($produit['date_fin_promo']) >= $date_achat) {
+                    $infos_promo['en_promo'] = true;
+                    $infos_promo['tarif_actif'] = $produit['tarif_promo'];
+                }
+
+                $produit = array_merge($produit, $infos_promo);
+                $facture_produit['produit'] = $produit;
+
+                //dd($facture_produit['produit']); //array ac infos produit en cours
+                //dd($facture_produit);  //entite facture_produit ac les infos produit editees OK
+                array_push($factures_produits_edite, $facture_produit);
+            }
+
+            //dd($factures_produits_edite); // OK comporte bien les infos voulues
+            $facture['produit'] = $factures_produits_edite;
+
+            //dd($facture); //OK
+
+            //test end
+
             $response = json_encode($facture);
             $response = new JsonResponse($response);
             return $response;
